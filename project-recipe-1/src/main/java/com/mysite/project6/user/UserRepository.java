d90421798c7f8de4fd6a7934d3d163aba4d31803
zmsejddl <167294821+zmsejddl@users.noreply.github.com>
2024-07-10 11:40:21 +0900

    사용자 정보 저장 기능 추가

--- project-recipe-1/src/main/java/com/mysite/project6/user/UserRepository.java
+++ project-recipe-1/src/main/java/com/mysite/project6/user/UserRepository.java
@@ -5,7 +5,6 @@ import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 
-public interface UserRepository extends JpaRepository<User, Integer>{
+public interface UserRepository extends JpaRepository<User, Long>{
 
-	Optional<User> findByUsername(String username);
 }

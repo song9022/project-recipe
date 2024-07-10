e412059b1926231c8febbb616ab96030a748c4ec
zmsejddl <167294821+zmsejddl@users.noreply.github.com>
2024-07-10 10:35:09 +0900

    이미지 엔티티 추가

---
+++ project-recipe-1/src/main/java/com/mysite/project6/FileUploadConfig.java
@@ -0,0 +1,16 @@
+//package com.mysite.project6;
+//
+////Application.java 혹은 원하는 설정 클래스에 아래 내용 추가
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.web.multipart.MultipartResolver;
+//import org.springframework.web.multipart.support.StandardServletMultipartResolver;
+//
+//@Configuration
+//public class FileUploadConfig {
+// 
+//	 @Bean
+//	    public MultipartResolver multipartResolver() {
+//	        return new StandardServletMultipartResolver();
+//	    }
+//}
---
+++ project-recipe-1/src/main/java/com/mysite/project6/image/ImageRepository.java
@@ -0,0 +1,7 @@
+package com.mysite.project6.image;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ImageRepository extends JpaRepository<Image, Integer>{
+
+}
